6c56d174d2b8dad5b3a610644a2d5c1773d4dcf1
ankithasn <sn.ankitha0003@gmail.com>
2022-11-25 15:23:09 +0550

    initial code

---
+++ dummy.py
@@ -0,0 +1,42 @@
+import os
+from git import Repo
+
+
+git_user_id = "ankithasn"
+git_repo_id = "OSB-newgit"
+COMMIT_MESSAGE = "initial code"
+Github_token = "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"
+
+PATH_OF_GIT_REPO = os.getcwd()
+# PATH_OF_GIT_REPO = r'/Users/ankitha/Documents/Baffle/repos/hackathon/javascript-client-generated/'
+
+
+def gitPushToRepo(git_user_id, git_repo_id, COMMIT_MESSAGE, Github_token):
+    # TODO encrypt github token
+
+    try:
+        repo = Repo.init(PATH_OF_GIT_REPO)
+        # repo.git.checkout("main")
+        repo.git.add(".")
+        print("added files")
+        # TODO undo git changes if error
+        repo.index.commit(COMMIT_MESSAGE)
+        print(git_user_id + ":" + Github_token + "@github.com/" + git_user_id + "/" + git_repo_id + ".git")
+        if "origin" not in repo.remotes:
+            # origin = repo.create_remote('origin', repo.remotes.origin.url)
+            o = repo.create_remote(name='origin', url=git_user_id + ":" + Github_token + "@github.com/" + git_user_id + "/" + git_repo_id + ".git")
+        else:
+            o = repo.remotes.origin
+        o.pull("main")
+        o.push()
+        # repo.git.push()
+    except Exception as e:
+        print('Some error occurred while pushing the code', e)
+
+
+if __name__ == "__main__":
+    # git_user_id = input("Enter user id: ")
+    # git_repo_id = input("Enter repo name: ")
+    # COMMIT_MESSAGE = input("Enter commit message: ")
+    # Github_token = input("Enter github token: ")
+    gitPushToRepo(git_user_id, git_repo_id, COMMIT_MESSAGE, Github_token)
---
+++ tets_push.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+# ref: https://help.github.com/articles/adding-an-existing-project-to-github-using-the-command-line/
+#
+# Usage example: /bin/sh ./git_push.sh wing328 swagger-petstore-perl "minor update"
+
+git_user_id=$1
+git_repo_id=$2
+release_note=$3
+
+if [ "$git_user_id" = "" ]; then
+    git_user_id="GIT_USER_ID"
+    echo "[INFO] No command line input provided. Set \$git_user_id to $git_user_id"
+fi
+
+if [ "$git_repo_id" = "" ]; then
+    git_repo_id="GIT_REPO_ID"
+    echo "[INFO] No command line input provided. Set \$git_repo_id to $git_repo_id"
+fi
+
+if [ "$release_note" = "" ]; then
+    release_note="Minor update"
+    echo "[INFO] No command line input provided. Set \$release_note to $release_note"
+fi
+echo "started"
+# Initialize the local directory as a Git repository
+#path="/Users/ankitha/Documents/Baffle/repos/hackathon/javascript-client-generated_test"
+#cd $path
+git init
+echo "stae"
+# Adds the files in the local repository and stages them for commit.
+git add .
+#git config --global user.email "sn.ankitha0003@gmail.com"
+#git config --global user.name "ankithasn"
+# Commits the tracked changes and prepares them to be pushed to a remote repository.
+git commit -m "$release_note"
+echo "ste"
+
+
+
+GIT_TOKEN="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"
+# Sets the new remote
+git_remote=`git remote`
+echo $git_remote
+echo "strat"
+echo "git remote add origin https://github.com/${git_user_id}/${git_repo_id}.git"
+git remote add origin https://github.com/${git_user_id}/${git_repo_id}.git
+#if [ "$git_remote" = "" ]; then # git remote not defined
+#    echo $GIT_TOKEN . "git token"
+#    if [ "$GIT_TOKEN" = "" ]; then
+#        echo "[INFO] \$GIT_TOKEN (environment variable) is not set. Using the git crediential in your environment."
+#        git remote add origin https://github.com/${git_user_id}/${git_repo_id}.git
+#    else
+#        echo "hew"
+#        echo "git remote add origin https://${git_user_id}:${GIT_TOKEN}@github.com/${git_user_id}/${git_repo_id}.git"
+#        git remote add origin https://${git_user_id}:${GIT_TOKEN}@github.com/${git_user_id}/${git_repo_id}.git
+#    fi
+#    #git branch -M master
+#fi
+
+git pull origin main
+
+# Pushes (Forces) the changes in the local repository up to the remote repository
+echo "Git pushing to https://github.com/${git_user_id}/${git_repo_id}.git"
+git push origin main 2>&1 | grep -v 'To https'
